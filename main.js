4aacc13ff5b342b046147f30a93ff81bb65e3e26
Dan Johnson <danimal.johnson@gmail.com>
2021-08-16 23:46:09 -0700

    Rebased app using different boilerplate code.

--- main.js
+++ main.js
@@ -1,71 +1,82 @@
-const { app, BrowserWindow, Menu } = require('electron')
-const log = require('electron-log')
-
-// Set env
-process.env.NODE_ENV = 'development'
-
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isMac = process.platform === 'darwin' ? true : false
+const path = require('path')
+const url = require('url')
+const { app, BrowserWindow } = require('electron')
 
 let mainWindow
 
-function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    title: 'APP NAME',
-    width: isDev ? 800 : 500,
-    height: 600,
-    icon: './assets/icons/icon.png',
-    resizable: isDev ? true : false,
-    backgroundColor: 'white',
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  })
+let isDev = false
 
-  if (isDev) {
-    mainWindow.webContents.openDevTools()
-  }
-
-  mainWindow.loadFile('./app/index.html')
+if (
+	process.env.NODE_ENV !== undefined &&
+	process.env.NODE_ENV === 'development'
+) {
+	isDev = true
 }
 
-app.on('ready', () => {
-  createMainWindow()
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		width: 1100,
+		height: 800,
+		show: false,
+		icon: `${__dirname}/assets/icon.png`,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
 
-  const mainMenu = Menu.buildFromTemplate(menu)
-  Menu.setApplicationMenu(mainMenu)
-})
+	let indexPath
 
-const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
-  {
-    role: 'fileMenu',
-  },
-  ...(isDev
-    ? [
-        {
-          label: 'Developer',
-          submenu: [
-            { role: 'reload' },
-            { role: 'forcereload' },
-            { type: 'separator' },
-            { role: 'toggledevtools' },
-          ],
-        },
-      ]
-    : []),
-]
+	if (isDev && process.argv.indexOf('--noDevServer') === -1) {
+		indexPath = url.format({
+			protocol: 'http:',
+			host: 'localhost:8080',
+			pathname: 'index.html',
+			slashes: true,
+		})
+	} else {
+		indexPath = url.format({
+			protocol: 'file:',
+			pathname: path.join(__dirname, 'dist', 'index.html'),
+			slashes: true,
+		})
+	}
+
+	mainWindow.loadURL(indexPath)
+
+	// Don't show until we are ready and loaded
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show()
+
+		// Open devtools if dev
+		if (isDev) {
+			const {
+				default: installExtension,
+				REACT_DEVELOPER_TOOLS,
+			} = require('electron-devtools-installer')
+
+			installExtension(REACT_DEVELOPER_TOOLS).catch((err) =>
+				console.log('Error loading React DevTools: ', err)
+			)
+			mainWindow.webContents.openDevTools()
+		}
+	})
+
+	mainWindow.on('closed', () => (mainWindow = null))
+}
+
+app.on('ready', createMainWindow)
 
 app.on('window-all-closed', () => {
-  if (!isMac) {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow()
-  }
+	if (mainWindow === null) {
+		createMainWindow()
+	}
 })
 
+// Stop error
 app.allowRendererProcessReuse = true
