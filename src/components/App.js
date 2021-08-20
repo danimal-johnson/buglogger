1313ff977e6ef26f0d4c864f555639d45263192f
Dan Johnson <danimal.johnson@gmail.com>
2021-08-20 00:45:22 -0700

    Add timed alert messages.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Table from 'react-bootstrap/Table';
+import { Container, Table, Alert } from 'react-bootstrap';
+// import Table from 'react-bootstrap/Table';
 import LogItem from './LogItem';
 import AddLogItem from './AddLogItem';
 
@@ -28,17 +28,45 @@ const App = () => {
 			created: new Date().toString(),
 		},
 	]);
+	const [alert, setAlert] = useState({
+		show: false,
+		message: '',
+		variant: 'success',
+	});
 
 	function addItem(item) {
+		if (item.text === '' || item.user === '' || item.priority === '') {
+			showAlert('Please fill in all fields.', 'danger');
+			return;
+		}
+
 		item._id = Math.floor(Math.random() * 90000) + 10000;
 		item.created = new Date().toString();
 		console.log(item);
 		setLogs([...logs,	item]);
+		showAlert('Log added successfully');
+	}
+
+	function removeItem(item) {
+	}
+
+	function showAlert(message, variant = 'success', seconds = 3) {
+		setAlert({
+			show: true,
+			message,
+			variant,
+		});
+		setTimeout(() => {
+			setAlert({
+				show: false,
+			});
+		}, seconds * 1000);
 	}
 
 	return (
 		<Container>
 			<AddLogItem addItem={addItem} />
+			{alert.show && <Alert variant={alert.variant}>{alert.message}</Alert>}
 			<Table>
 				<thead>
 					<tr>
